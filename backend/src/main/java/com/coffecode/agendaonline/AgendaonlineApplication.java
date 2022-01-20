04c9efcb64267edb62aa2f801512b2b155dc8c7f
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2022-01-19 23:35:26 -0300

    Projeto criado

---
+++ backend/src/main/java/com/coffecode/agendaonline/AgendaonlineApplication.java
@@ -0,0 +1,13 @@
+package com.coffecode.agendaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaonlineApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/coffecode/agendaonline/AgendaonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffecode.agendaonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
