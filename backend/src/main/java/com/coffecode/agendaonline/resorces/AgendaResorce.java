7981c25b1f5b26a9d154903931fed898b4a21a8e
Henrique Borsoi Lopes <henriqueborsoilopes@gmail.com>
2022-02-03 18:37:10 -0300

    Atualizando retorno das funções Agenda

--- backend/src/main/java/com/coffecode/agendaonline/resorces/AgendaResorce.java
+++ backend/src/main/java/com/coffecode/agendaonline/resorces/AgendaResorce.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,23 +23,26 @@ public class AgendaResorce {
 	private AgendaService service;
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public void insert(@Valid @RequestBody Agenda obj) {
+	public ResponseEntity<Void> insert(@Valid @RequestBody Agenda obj) {
 		service.insert(obj);
+		return ResponseEntity.ok().build();
 	}
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public List<Agenda> findAll(){
+	public ResponseEntity<List<Agenda>> findAll(){
 		List<Agenda> agendas = service.findAll();
-		return agendas;
+		return ResponseEntity.ok().body(agendas);
 	}
 	
 	@RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
-	public void delete(@PathVariable Long id) {
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
+		return ResponseEntity.ok().build();
 	}
 	
 	@RequestMapping(path = "/{id}", method = RequestMethod.PUT)
-	public void update(@Valid @RequestBody Agenda obj, @PathVariable Long id) {
+	public ResponseEntity<Void> update(@Valid @RequestBody Agenda obj, @PathVariable Long id) {
 		service.update(obj, id);
+		return ResponseEntity.ok().build();
 	}
 }
